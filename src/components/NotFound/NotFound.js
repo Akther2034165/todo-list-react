8f1f2dc0462fa04fc5cfc3533bc843777d894cb2
Akther Hosenn <nirjhorn457@gmail.com>
2021-10-02 23:19:17 +0600

    first commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import img from "../../Media/404page.JPG";
+const NotFound = () => {
+  return (
+    <div>
+      <img height="250px" src={img} alt="" />
+      <h2>Page Not Found!!!</h2>
+    </div>
+  );
+};
+
+export default NotFound;
